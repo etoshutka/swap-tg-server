70de396220c3045c724528499bd35ea78b7a20cf
etoshutka <cheatmaster114@gmail.com>
2024-09-18 21:35:55 +0300

    Uncomment AuthGuard

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -6,15 +6,15 @@ import { Request } from "express";
 @Injectable()
 export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    // const request: Request = context.switchToHttp().getRequest();
-    // const cookies: CookieKeys = transformCookieToObject(request.headers.cookie);
-    //
-    // const csrfTokenFromCookie: string = cookies?.CSRF_TOKEN;
-    // const csrfTokenFromClient: string = cookies?.CSRF_CLIENT_TOKEN;
-    //
-    // if (!csrfTokenFromClient || csrfTokenFromCookie !== csrfTokenFromClient) {
-    //   throw new ForbiddenException("Access denied");
-    // }
+    const request: Request = context.switchToHttp().getRequest();
+    const cookies: CookieKeys = transformCookieToObject(request.headers.cookie);
+    
+    const csrfTokenFromCookie: string = cookies?.CSRF_TOKEN;
+    const csrfTokenFromClient: string = cookies?.CSRF_CLIENT_TOKEN;
+    
+    if (!csrfTokenFromClient || csrfTokenFromCookie !== csrfTokenFromClient) {
+      throw new ForbiddenException("Access denied");
+    }
 
     return true;
   }
