d1275a2acbc6915e1a003e7977531d066071beb5
etoshutka <cheatmaster114@gmail.com>
2024-09-19 13:13:26 +0300

    Try to fix middleware

--- src/common/middlewares/auth.middleware.ts
+++ src/common/middlewares/auth.middleware.ts
@@ -2,7 +2,7 @@ import { transformCookieToObject } from "../helpers/transformCookieToObject";
 import { COOKIE_KEYS, CookieKeys } from "../consts/cookie-keys.const";
 import { COOKIE_CONFIG } from "../consts/cookie-config.const";
 import { UserModel, UsersService } from "src/domains/users";
-import { Injectable, NestMiddleware } from "@nestjs/common";
+import { Injectable, NestMiddleware, UnauthorizedException } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 import { v4 as uuidv4 } from "uuid";
 
@@ -14,21 +14,30 @@ export class AuthMiddleware implements NestMiddleware {
     console.log('AuthMiddleware: Headers:', req.headers);
     const cookie: CookieKeys = transformCookieToObject(req.headers.cookie);
 
+    try {
+      if (!cookie?.CSRF_TOKEN && req.query.telegram_id) {
+        const csrfToken: string = uuidv4();
+        res.cookie(COOKIE_KEYS.CSRF_TOKEN, csrfToken, COOKIE_CONFIG);
+        res.cookie(COOKIE_KEYS.CSRF_CLIENT_TOKEN, csrfToken);
+        const user = await this.userService.findOne({ telegram_id: req.query.telegram_id as string });
+        if (user) {
+          await this.userService.updateOne({ id: user.id, csrf_token: csrfToken });
+          req.user = user;
+        }
+      } else if (cookie?.CSRF_TOKEN) {
+        req.user = await this.userService.findOne({ csrf_token: cookie.CSRF_TOKEN });
+      }
 
-    if (!cookie?.CSRF_TOKEN && req.query.telegram_id) {
-      const csrfToken: string = uuidv4();
-      res.cookie(COOKIE_KEYS.CSRF_TOKEN, csrfToken, COOKIE_CONFIG);
-      res.cookie(COOKIE_KEYS.CSRF_CLIENT_TOKEN, csrfToken);
-      const user = await this.userService.findOne({ telegram_id: req.query.telegram_id as string });
-      await this.userService.updateOne({ id: user.id, csrf_token: csrfToken });
-      req.user = user;
+      console.log('AuthMiddleware: req.user:', req.user);
+
+      if (!req.user && req.path !== '/v1/auth/login') {
+        throw new UnauthorizedException('User not authenticated');
+      }
+
+      next();
+    } catch (error) {
+      console.error('AuthMiddleware Error:', error);
+      res.status(401).json({ message: 'Unauthorized' });
     }
-
-    if (cookie?.CSRF_TOKEN) {
-      req.user = await this.userService.findOne({ csrf_token: cookie.CSRF_TOKEN });
-    }
-    console.log('AuthMiddleware: req.user:', req.user);
-
-    next();
   }
 }
