8d8154ad6376df25bef7a2065c724bc55577e32b
etoshutka <cheatmaster114@gmail.com>
2024-10-08 11:15:51 +0300

    add slippage to swap

--- src/domains/wallets/usecases/services/sdk.service.ts
+++ src/domains/wallets/usecases/services/sdk.service.ts
@@ -833,7 +833,7 @@ export class SdkService {
             fromTokenAddress || "So11111111111111111111111111111111111111112",
             toTokenAddress || "So11111111111111111111111111111111111111112",
             Number(amount),
-            100
+            500
           );
         
           //console.log("Slippage (bps):", slippageBps);
